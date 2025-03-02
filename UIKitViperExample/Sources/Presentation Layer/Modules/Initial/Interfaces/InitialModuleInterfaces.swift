00a208f1a804d91d3100709c0956dc667238514c
Егор Скляров <red.swift247@gmail.com>
2025-03-02 22:12:28 +0400

    add initial screen module

---
+++ UIKitViperExample/Sources/Presentation Layer/Modules/Initial/Interfaces/InitialModuleInterfaces.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+// MARK: - Interactor
+
+protocol InitialModuleInteractorInput: ModuleInteractorInput {
+    
+}
+
+protocol InitialModuleInteractorOutput: ModuleInteractorOutput {
+    
+}
+
+// MARK: - Routing
+
+protocol InitialModuleRouterInput: ModuleRouterInput {
+    
+}
+
+// MARK: - View
+
+protocol InitialModuleViewInput: ModuleViewInput {
+    
+}
+
+protocol InitialModuleViewOutput: ModuleViewOutput {
+    
+}
---
+++ UIKitViperExample/Sources/Presentation Layer/Modules/Initial/Router/InitialModuleRouter.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class InitialModuleRouter: BaseRouter {
+    
+}
+
+// MARK: - InitialModuleRouterInput
+
+extension InitialModuleRouter: InitialModuleRouterInput {
+    
+}
