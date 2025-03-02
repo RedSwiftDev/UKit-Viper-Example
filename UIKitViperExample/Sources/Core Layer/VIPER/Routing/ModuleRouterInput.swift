fa3ff9a7fe5b2e5a684db1c0e9a4f3292a060984
Егор Скляров <red.swift247@gmail.com>
2025-03-02 23:01:34 +0400

    ModuleRouterInput: add tab bar methods

--- UIKitViperExample/Sources/Core Layer/VIPER/Routing/ModuleRouterInput.swift
+++ UIKitViperExample/Sources/Core Layer/VIPER/Routing/ModuleRouterInput.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 @MainActor protocol ModuleRouterInput: AnyObject {
+    
+    // MARK: UIViewController
+    
     @MainActor func present(viewController: UIViewController, animated: Bool)
     @MainActor func present(
         viewController: UIViewController,
@@ -10,6 +13,8 @@ import UIKit
     
     @MainActor func dismiss(animated: Bool)
     @MainActor func dismiss(animated: Bool, completion: @escaping () -> Void)
+    
+    // MARK: UINavigationController
     
     @MainActor func open(viewController: UIViewController, animated: Bool)
     @MainActor func open(viewControllers: UIViewController..., animated: Bool)
@@ -22,4 +27,14 @@ import UIKit
     @MainActor func pop(animated: Bool)
     @MainActor func pop(to viewController: UIViewController, animated: Bool)
     @MainActor func popToRoot(animated: Bool)
+    
+    // MARK: UITabBarController
+    
+    @MainActor func appendTab(viewController: UIViewController, animated: Bool)
+    @MainActor func insertTab(
+        viewController: UIViewController,
+        at index: Int,
+        animated: Bool
+    )
+    @MainActor func removeTab(viewController: UIViewController, animated: Bool)
 }
